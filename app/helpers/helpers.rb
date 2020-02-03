56ede3064c8f1cc94af122f1b7ff5f10963394ae
Justin Miller <jayman1294@gmail.com>
2020-02-03 08:12:21 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+  
+  def self.current_user(session)
+   @current_user = User.find(session[:user_id])
+  end 
+  
+  def self.is_logged_in?(session)
+    @logged_in = User.find(session[:user_id])
+    if @logged_in != nil
+      true
+    else 
+      false
+    end
+  end
 end
