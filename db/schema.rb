b1c975945fd12c6880880241e9381070de02dc6b
Adrian Bruno <adrian.j.bruno@gmail.com>
2020-02-06 22:59:46 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200205214218) do
+ActiveRecord::Schema.define(version: 20200206225650) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.integer "balance"
+    t.decimal "balance"
   end
 
 end
